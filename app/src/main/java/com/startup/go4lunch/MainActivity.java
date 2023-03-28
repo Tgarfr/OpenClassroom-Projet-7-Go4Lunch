e1c0ac542fd61a24d89dddbb0a9fb381cd45286d
Thomas Garandel <tgarfr@laposte.net>
2023-03-28 14:43:58 +0200

    Add Authentication with Email and Google

--- app/src/main/java/com/startup/go4lunch/MainActivity.java
+++ app/src/main/java/com/startup/go4lunch/MainActivity.java
@@ -1,9 +1,11 @@
 package com.startup.go4lunch;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -11,26 +13,66 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.AuthUI.IdpConfig;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.startup.go4lunch.di.ViewModelFactory;
 import com.startup.go4lunch.ui.MainActivityViewModel;
 import com.startup.go4lunch.ui.ViewPagerAdapter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel viewModel;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(MainActivityViewModel.class);
+        firebaseAuth = FirebaseAuth.getInstance();
+        verifyFirebaseUser();
         getCurrentLocation();
         configureViewPager();
+    }
+
+    private void verifyFirebaseUser() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            startSignInActivity();
+        }
+    }
+
+    ActivityResultLauncher<Intent> signInLuncher = registerForActivityResult(new FirebaseAuthUIActivityResultContract(), this::onSignInResult);
+
+    private void startSignInActivity() {
+        List<IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLuncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        if (result.getResultCode() == RESULT_OK) {
+             // TODO : Create Workmate
+        } else {
+            startSignInActivity();
+        }
     }
 
     private void getCurrentLocation() {
